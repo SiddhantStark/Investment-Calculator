89b7adcff55bf491632a860a03f71ce0a624a913
Siddhant Prasad <siddhantprasad.tvnl@gmail.com>
2024-11-24 21:55:49 +0530

    app completed using standalone components and decorators

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+
+  @Output() calculate = new EventEmitter<InvestmentInput>
+
+  initialInvestment = 0
+  annualInvestment = 0
+  expectedReturn = 0
+  duration = 5
+
+
+  onSubmit(){
+    // console.log("Hi there");
+    // console.log(this.initialInvestment);
+    // console.log(this.annualInvestment);
+    // console.log(this.expectedReturn);
+    // console.log(this.duration);
+    this.calculate.emit({
+      initialInvestment: +this.initialInvestment,
+      annualInvestment: +this.annualInvestment,
+      expectedReturn: +this.expectedReturn,
+      duration: +this.duration
+    })
+  }
 
 }
